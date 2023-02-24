79d0ec292572ac19f357a87ea443a20c42eccb05
a4b11s <a4b11s@gmail.com>
2023-02-24 22:26:36 +0200

    style: formatting

--- backend/routes/img.js
+++ backend/routes/img.js
@@ -1,22 +1,20 @@
-const express = require("express");
-const compressFiles = require("../services/compress");
+const express = require('express');
+const compressFiles = require('../services/compress');
 const router = express.Router();
 
-
-router.post("/", function (req, res, next) {
-	res.set("Access-Control-Allow-Origin", "*");
-	try {
-		if (req.files) {
-			compressFiles(Object.values(req.files)).then(data=>{
-				console.log(data)
-				res.type("application/zip")
-				res.send(data)
-			})
-		}
-	}catch (e){
-		console.log(e)
-		res.sendStatus(500)
-	}
+router.post('/', function (req, res) {
+  try {
+    if (req.files) {
+      compressFiles(Object.values(req.files)).then((data) => {
+        console.log(data);
+        res.type('application/zip');
+        res.send(data);
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
 });
 
 module.exports = router;
--- backend/services/compress.js
+++ backend/services/compress.js
@@ -1,26 +1,23 @@
-const JSZip = require("jszip");
+const JSZip = require('jszip');
 
 const zip = new JSZip();
 
-
 async function filesArray2Zip(filesArray) {
-    if(filesArray?.length){
-        await filesArray.map((item) => {
-            zip.file(item.name, item.data);
-        });
-    }else {
-        zip.file(filesArray.name, filesArray.data);
-    }
+  if (filesArray?.length) {
+    await filesArray.map((item) => {
+      zip.file(item.name, item.data);
+    });
+  } else {
+    zip.file(filesArray.name, filesArray.data);
+  }
 }
 
 async function compressFiles(files) {
-    let compress = await filesArray2Zip(files).then(() => {
-        return zip.generateAsync({type: "arraybuffer"}).then((buffer) => {
-            return buffer
-        });
-
+  return await filesArray2Zip(files).then(() => {
+    return zip.generateAsync({ type: 'arraybuffer' }).then((buffer) => {
+      return buffer;
     });
-    return compress
+  });
 }
 
-module.exports = compressFiles
+module.exports = compressFiles;

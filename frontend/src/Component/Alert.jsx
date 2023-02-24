3c0ad768e04e1811303aeec68d875b49d05a8ea7
a4b11s <a4b11s@gmail.com>
2023-02-24 22:11:35 +0200

    style: formatting

--- frontend/src/Component/Alert.jsx
+++ frontend/src/Component/Alert.jsx
@@ -1,23 +1,23 @@
-import React from "react";
-import PropTypes from "prop-types";
-import style from "./Alert.module.css";
-
+import React from 'react';
+import PropTypes from 'prop-types';
+import style from './Alert.module.css';
 
 Alert.propTypes = {
-    message: PropTypes.string,
-    isOpen: PropTypes.bool,
+  message: PropTypes.string,
+  isOpen: PropTypes.bool,
 };
 
-export default function Alert({isOpen,message}) {
-    if (isOpen){
-        return (<div className={style.wrapper}>
-            <svg className={style.icon} focusable="false" aria-hidden="true"
-                 viewBox="0 0 24 24" >
-                <path
-                    d="M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z"></path>
-            </svg>
-            <span>{message}</span></div>);
-    }else {
-        return null
-    }
+export default function Alert({ isOpen, message }) {
+  if (isOpen) {
+    return (
+      <div className={style.wrapper}>
+        <svg className={style.icon} focusable='false' aria-hidden='true' viewBox='0 0 24 24'>
+          <path d='M11 15h2v2h-2zm0-8h2v6h-2zm.99-5C6.47 2 2 6.48 2 12s4.47 10 9.99 10C17.52 22 22 17.52 22 12S17.52 2 11.99 2zM12 20c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8z'></path>
+        </svg>
+        <span>{message}</span>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }

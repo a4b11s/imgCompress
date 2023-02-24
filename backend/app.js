2ee95e35892c8eea273700991662e49c9b76df23
a4b11s <a4b11s@gmail.com>
2023-02-24 22:27:03 +0200

    refactor

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,21 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const fileUpload = require("express-fileupload");
-
-const imgRouter = require("./routes/img");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const fileUpload = require('express-fileupload');
+const imgRouter = require('./routes/img');
+const {accessControl} = require("./middleware/accessControl");
 
 const app = express();
 
-app.use(logger("dev"));
+const port = parseInt(process.env.PORT || '3002');
+app.use(logger('dev'));
+
 app.use(express.json());
-app.use(fileUpload());
+app.use(fileUpload({}));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use("/api/img", imgRouter);
-
-module.exports = app;
+app.use(accessControl);
+app.use('/api/img', imgRouter);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

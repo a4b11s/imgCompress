538b271be4619c0fbcc81338e11ce1717b0e4b3c
a4b11s <a4b11s@gmail.com>
2023-02-24 22:09:29 +0200

    style: formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,57 +1,68 @@
-import "./App.css";
-import ImgInput from "./Component/ImgInput";
-import React, { useState } from "react";
-import Spinner from "./Component/Spinner";
-import Alert from "./Component/Alert";
+import React, { useState } from 'react';
 
+import ImgInput from './Component/ImgInput';
+import Spinner from './Component/Spinner';
+import Alert from './Component/Alert';
 
+import './App.css';
+
+const url =
+  process.env.REACT_APP_BACKEND_HOST + process.env.REACT_APP_BACKEND_IMG_COMPRESS_ENDPOINT;
 
 function App() {
-	const [isUploading, setIsUploading] = useState(false);
-	const [uploadData, setUploadData] = useState("");
-	const [isError, setIsError] = useState(false);
+  const [isUploading, setIsUploading] = useState(false);
+  const [uploadData, setUploadData] = useState('');
+  const [isError, setIsError] = useState(false);
 
-	function handleError() {
-		setIsError(true)
-		setIsUploading(false)
-		setTimeout(()=>setIsError(false), 8000)
-	}
+  function handleError(e) {
+    setIsError(true);
+    setIsUploading(false);
+    console.log(e);
+    setTimeout(() => setIsError(false), 8000);
+  }
 
+  function handleFile(files) {
+    const formData = new FormData();
 
-	function handleFile(files) {
-		setUploadData("");
-		const formData = new FormData();
-		for (let i = 0; i < files.length; i++) {
-			formData.append(files[i].name, files[i]);
-		}
-		setIsUploading(true);
-		fetch(process.env.REACT_APP_BACKEND_HOST+process.env.REACT_APP_BACKEND_IMG_COMPRESS_ENDPOINT, {
-			method: "POST",
-			body: formData,
-		}).then((data) => {
-			data.blob().then((blob) => {
-				const objectURL = URL.createObjectURL(blob);
-				setUploadData(objectURL);
-				setIsUploading(false);
-			});
-		}).catch(err=>{
-			console.log(err)
-			handleError(err)
-		});
-	}
+    setUploadData('');
+    setIsUploading(true);
 
+    for (let i = 0; i < files.length; i++) {
+      formData.append(files[i].name, files[i]);
+    }
 
-	return (
-		<div className="container">
-			{isUploading ? <Spinner /> : <ImgInput callback={handleFile} />}
-			<Alert isOpen={isError} message="Fetching error"/>
-			{uploadData && (
-				<a className="downloadBtn" href={uploadData}>
-					Download
-				</a>
-			)}
-		</div>
-	);
+    fetch(url, {
+      method: 'POST',
+      body: formData,
+    })
+      .then((data) => {
+        data
+          .arrayBuffer()
+          .then((buffer) => {
+            let blob = new Blob(new Uint8Array(buffer));
+            setUploadData(blob);
+            setIsUploading(false);
+          })
+          .catch((e) => {
+            handleError(e);
+          });
+      })
+      .catch((e) => {
+        handleError(e);
+      });
+  }
+
+  return (
+    <div className='container'>
+      {isUploading ? <Spinner /> : <ImgInput callback={handleFile} />}
+      <Alert isOpen={isError} message='Fetching error' />
+      {uploadData && (
+        <a download='image.zip' className='downloadBtn' href={uploadData}>
+          Download
+        </a>
+      )}
+    </div>
+  );
 }
 
 export default App;
